3e2936e2d7062166fb9ccfa9fed55f17e3b9179e
Brett-Kellz94 <brett.kelley@hotmail.com>
2020-12-14 17:39:41 -0500

    completed dto

--- CookBook/src/main/java/com/revature/CookBook/Dto/RecipeDto.java
+++ CookBook/src/main/java/com/revature/CookBook/Dto/RecipeDto.java
@@ -1,8 +1,5 @@
 package com.revature.CookBook.Dto;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 import com.revature.CookBook.pojos.Recipe;
 import com.revature.CookBook.pojos.User;
@@ -17,15 +14,15 @@ public class RecipeDto implements Dto<Recipe>{
 	
 	private String name;
 	
-	private String user;
+	private String author;
 	
 	private String featured;
 	
-	private String servings;
+	private int servings;
 	
-	private String prepTime;
+	private int prepTime;
 	
-	private String cookTime;
+	private int cookTime;
 
 	private String steps;
 	
@@ -35,22 +32,24 @@ public class RecipeDto implements Dto<Recipe>{
 	
 	private String description;
 	
-	//private String recipeImage;
+	private byte[] recipeImage;
 	
 	
 	public RecipeDto(Recipe recipe) {
 		super();
 		this.name= recipe.getName();
 		this.featured = Boolean.toString(recipe.isFeatured());
-		this.servings= Integer.toString(recipe.getServings());
-		this.prepTime=Integer.toString(recipe.getPrepTime());
-		this.cookTime=Integer.toString(recipe.getCookTime());
+		this.servings= recipe.getServings();
+		this.prepTime=recipe.getPrepTime();
+		this.cookTime=recipe.getCookTime();
 		this.steps= recipe.getSteps();
 		this.tags=recipe.getTags();
 		this.ingredients=recipe.getIngredients();
 		this.description=recipe.getDescription();
-		//this.recipeImage=recipe.getRecipeImage();
-		this.user=Integer.toString(recipe.getUser().getUserId());
+		this.recipeImage=recipe.getRecipeImage();
+		this.author= recipe.getUser().getUsername();
+		
+		
 	}
 
 	@Override
@@ -58,17 +57,18 @@ public class RecipeDto implements Dto<Recipe>{
 		Recipe recipe = new Recipe();
 		recipe.setName(this.getName());
 		recipe.setFeatured(Boolean.parseBoolean(this.getFeatured()));
-		recipe.setServings(Integer.parseInt(this.getServings()));
-		recipe.setPrepTime(Integer.parseInt(this.getPrepTime()));
-		recipe.setCookTime(Integer.parseInt(this.getCookTime()));
+		recipe.setServings(this.getServings());
+		recipe.setPrepTime(this.getPrepTime());
+		recipe.setCookTime(this.getCookTime());
 		recipe.setSteps(this.getSteps());
 		recipe.setTags(this.getTags());
 		recipe.setIngredients(this.getIngredients());
 		recipe.setDescription(this.getDescription());
 		
 		User user= new User();
-		//recipe.setUser(this.getUser());
-		
-		return null;
+		user.setUsername(this.author);
+		recipe.setUser(user);
+	
+		return recipe;
 	}
 }
