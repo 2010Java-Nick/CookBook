263fcb3bd128d40ae668707472dccd2f08b1429b
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2020-12-14 16:48:59 -0500

    Front end base components

--- CookBookUIng/src/app/app.module.ts
+++ CookBookUIng/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { SiteComponent } from './components/site/site.component';
+import { NavbarComponent } from './components/site/navbar/navbar.component';
+import { SidebarComponent } from './components/site/sidebar/sidebar.component';
+import { TitleComponent } from './components/landing/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SiteComponent,
+    NavbarComponent,
+    SidebarComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
