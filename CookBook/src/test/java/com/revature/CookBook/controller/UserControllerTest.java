9e6da5e12db69b1a094c677ffbc4ed7108970a9c
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2020-12-17 20:35:21 -0500

    Removed unused imports

--- CookBook/src/test/java/com/revature/CookBook/controller/UserControllerTest.java
+++ CookBook/src/test/java/com/revature/CookBook/controller/UserControllerTest.java
@@ -1,14 +1,14 @@
 package com.revature.CookBook.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.CookBook.daos.UserDao;
+import com.revature.CookBook.dto.UserDto;
+import com.revature.CookBook.pojos.User;
+import com.revature.CookBook.service.UserService;
 
 import org.hibernate.SessionFactory;
 import org.junit.After;
@@ -19,32 +19,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.CookBook.config.AppConfig;
-import com.revature.CookBook.daos.UserDao;
-import com.revature.CookBook.dto.UserDto;
-import com.revature.CookBook.pojos.User;
-import com.revature.CookBook.service.UserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { UserControllerTest.TestConfig.class })
