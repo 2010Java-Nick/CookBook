6fa621ef402e2fdb9491d256faf9416fe5655ea2
Brett-Kellz94 <brett.kelley@hotmail.com>
2020-12-20 17:21:11 -0500

    get all cookbooks works

--- CookBook/src/main/java/com/revature/CookBook/controller/CookBookController.java
+++ CookBook/src/main/java/com/revature/CookBook/controller/CookBookController.java
@@ -2,45 +2,43 @@ package com.revature.CookBook.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.revature.CookBook.dto.CookBookDto;
-import com.revature.CookBook.dto.RecipeDto;
 import com.revature.CookBook.pojos.CookBook;
-import com.revature.CookBook.pojos.Recipe;
 import com.revature.CookBook.service.CookBookService;
 
 @RestController
 public class CookBookController {
 
 	CookBookService cookBookService;
+
 	
 	@Autowired
 	public void setCookBookService(CookBookService cookBookService) {
 		this.cookBookService = cookBookService;
 	}
 	
-	//will need to call suer service to make a user for author
+    //will need to call suer service to make a user for author
 	//Create recipe
-//	@CrossOrigin(origins = "http://localhost:4200")
-//	@PostMapping("recipe")
-//	public void createRecipe(@RequestBody RecipeDto recipeDto) {
-//		System.out.println(recipeDto.toString());
-//		Recipe recipe = recipeDto.toPojo();
-//		User user = userService.readUser(recipeDto.getAuthor());
-//		recipe.setUser(user);
-//		boolean result = recipeService.createRecipe(recipe);
-//		ResponseEntity<RecipeDto> re = new ResponseEntity<RecipeDto>(HttpStatus.CREATED);
-//
-//	}
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("cookbook")
+	public void createCookBook(@RequestBody CookBookDto cookBookDto) {
+		
+		CookBook cookBook = cookBookDto.toPojo();
+		//User user = userService.readUser(cookBookDto.getAuthor());
+		//cookBook.setUser(user);
+		boolean result = cookBookService.createCookBook(cookBook);
+		ResponseEntity<CookBookDto> re = new ResponseEntity<CookBookDto>(HttpStatus.CREATED);
+	}
 
 	@RequestMapping(path = "cookbook/{cookbookId}", method = RequestMethod.GET)
 	public CookBookDto readCookBook(@PathVariable(name = "cookbookId") int cookBookId) {
